c929cdaad63e18030b94eb0a4f5e92ecc50389d5
m4md24 <m4md24@gmail.com>
2025-06-26 21:16:15 +0300

    Learn (Where Statement and Logical Operators) Concept

---
+++ src/_1_concepts/_1_8_sql_queries/_1_8_4_where_statement_and_logical_operators/WhereStatementAndLogicalOperators.sql
@@ -0,0 +1,52 @@
+USE HR_Database
+
+USE HR_Database;
+EXEC sp_rename 'HR_Database.dbo.Employees.Gendor', 'Gender', 'COLUMN'
+GO
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE MonthlySalary > 500
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE NOT MonthlySalary <= 500
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE NOT MonthlySalary <= 500
+  AND Gender = 'M'
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE CountryID = 1
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE NOT CountryID = 1
+
+-- OR
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE CountryID <> 1
+
+-- OR
+
+SELECT ID, FirstName, MonthlySalary
+FROM Employees
+WHERE CountryID != 1
+
+SELECT ID, FirstName, MonthlySalary, DepartmentID, CountryID
+FROM Employees
+WHERE CountryID = 1
+   OR DepartmentID = 2
+
+SELECT ID, FirstName
+FROM Employees
+WHERE ExitDate IS NULL
+
+SELECT ID, FirstName
+FROM Employees
+WHERE ExitDate IS NOT NULL
+
